4604669bbf88d6eff67bf3874b9ada1a0c2fa384
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-05-23 21:22:13 +0530

    update

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -18,7 +18,7 @@ export const projects = [
 		technologies: ['HTML', 'CSS', 'javaScript'],
 	},
 	{
-		img: '../../Screenshot (56).png',
+		img: 'https://www.flickr.com/photos/195713921@N06/52093819856/in/dateposted-public/',
 		title: 'Todo Application',
 		des: 'This is a small Todo Application built with React fronted technology. User can use simple Todo Application. For rendering a default data, I have used Local Storage. I have added Pagination feature as well. In this project, I have added complete and clear function through which User can crosses out completed Todo or delete it completely.',
 		// demo: 'https://bobbi-brown-project-8xs15wibv-mohak-tripathi.vercel.app/',
@@ -28,15 +28,6 @@ export const projects = [
 	},
 
 	
-	// {
-	// 	img: 'https://camo.githubusercontent.com/b7f4aa8c34ddf776dd19b78dc3488456470a73e5026f48784e9e753601468802/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4c62507030307771454537386c6f68584d372f67697068792e676966',
-	// 	title: 'Clone of Fitness Blender',
-	// 	des: 'This is a clone of the Fitness Blender website. The clone is complete with a Login page, Sign-in page, Shopping Cart, Wishlist and Calendar Scheduling feature. Following is the link to the clone of the website.',
-	// 	demo: '',
-	// 	live: 'https://anshusagar.github.io/FitnessBlender/',
-	// 	gitHub: 'https://github.com/Anshusagar/FitnessBlender.git',
-	// 	technologies: ['HTML', 'CSS', 'javaScript'],
-	// },
 	// {
 	// 	img: 'https://github.com/mandarsatam/Mailchimp-Clone/blob/master/Snips/Screenshot%20(79).png?raw=true',
 	// 	title: 'Mailchimp Clone',
